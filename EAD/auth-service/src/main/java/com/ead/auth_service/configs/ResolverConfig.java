bc7bdc6bf5c2baeafac5ac4d587e275e8c1f9946
Ramon <ramonsilva12305@outlook.com>
2024-11-23 20:57:48 -0300

    feat/Global CORS config with the ResolverConfig for all resources

--- EAD/auth-service/src/main/java/com/ead/auth_service/configs/ResolverConfig.java
+++ EAD/auth-service/src/main/java/com/ead/auth_service/configs/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,10 +15,16 @@ public class ResolverConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         var pageableResolver = new PageableHandlerMethodArgumentResolver();
-        pageableResolver.setFallbackPageable(PageRequest.of(0, 2));
+        pageableResolver.setFallbackPageable(PageRequest.of(0, 10));
         argumentResolvers.add(pageableResolver);
-
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/users/**").allowedOrigins("https");
+//        registry.addMapping("/auth/**").allowedOrigins("https");
+        registry.addMapping("/**").maxAge(3600);
+
+    }
 
 }
--- EAD/auth-service/src/main/java/com/ead/auth_service/controllers/UserController.java
+++ EAD/auth-service/src/main/java/com/ead/auth_service/controllers/UserController.java
@@ -6,8 +6,6 @@ import com.ead.auth_service.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
