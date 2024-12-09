9b7d58ae395d66307b192037ce6ee2d26f3041d4
Ramon <ramonsilva12305@outlook.com>
2024-12-08 22:31:05 -0300

    feat/Adding EnableSpringDataWebSupport for better visualization for pagination response

--- EAD/auth-service/src/main/java/com/ead/auth_service/configs/ResolverConfig.java
+++ EAD/auth-service/src/main/java/com/ead/auth_service/configs/ResolverConfig.java
@@ -4,13 +4,17 @@ import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ResolverConfig implements WebMvcConfigurer {
 
     @Override
