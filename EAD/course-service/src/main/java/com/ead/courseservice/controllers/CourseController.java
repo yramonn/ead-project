a3ab72ebed618db97b688486250c26b5978fcf7a
Ramon <ramonsilva12305@outlook.com>
2024-12-14 16:46:15 -0300

    feat/implementation of Composition Pattern for getAllCourses endpoint

--- EAD/course-service/src/main/java/com/ead/courseservice/controllers/CourseController.java
+++ EAD/course-service/src/main/java/com/ead/courseservice/controllers/CourseController.java
@@ -37,8 +37,13 @@ public class CourseController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec, Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec, pageable));
+    public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec,
+                                                           Pageable pageable,
+                                                           @RequestParam(required = false)UUID userId) {
+        Page<CourseModel> courseModelPage = (userId != null)
+                ? courseService.findAll(SpecificationTemplate.courseUserId(userId).and(spec), pageable)
+                : courseService.findAll(spec, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(courseModelPage);
     }
 
     @GetMapping("/{courseId}")
--- EAD/course-service/src/main/java/com/ead/courseservice/specifications/SpecificationTemplate.java
+++ EAD/course-service/src/main/java/com/ead/courseservice/specifications/SpecificationTemplate.java
@@ -1,9 +1,11 @@
 package com.ead.courseservice.specifications;
 
 import com.ead.courseservice.models.CourseModel;
+import com.ead.courseservice.models.CourseUserModel;
 import com.ead.courseservice.models.LessonModel;
 import com.ead.courseservice.models.ModuleModel;
 import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
@@ -47,6 +49,14 @@ public class SpecificationTemplate {
                         Root<ModuleModel> module = query.from(ModuleModel.class);
                         Expression<Collection<LessonModel>> moduleLessons = module.get("lessons");
                         return cb.and(cb.equal(module.get("moduleId"), moduleId), cb.isMember(lesson, moduleLessons));
+                };
+        }
+
+        public static Specification<CourseModel> courseUserId(final UUID userId) {
+                return (root, query, cb) -> {
+                        query.distinct(true);
+                        Join<CourseModel, CourseUserModel> courseJoin = root.join("courseUsers");
+                        return cb.equal(courseJoin.get("userId"), userId);
                 };
         }
 }
