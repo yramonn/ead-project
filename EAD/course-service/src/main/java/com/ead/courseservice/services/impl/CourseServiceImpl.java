c3c36c295dc0026cd40280e444e7b850db9fabc1
Ramon <ramonsilva12305@outlook.com>
2025-01-17 16:12:26 -0300

    feat/Deleting resources between auth and course microservices

--- EAD/course-service/src/main/java/com/ead/courseservice/services/impl/CourseServiceImpl.java
+++ EAD/course-service/src/main/java/com/ead/courseservice/services/impl/CourseServiceImpl.java
@@ -3,9 +3,11 @@ package com.ead.courseservice.services.impl;
 import com.ead.courseservice.dtos.CourseRecordDto;
 import com.ead.courseservice.exceptions.NotFoundException;
 import com.ead.courseservice.models.CourseModel;
+import com.ead.courseservice.models.CourseUserModel;
 import com.ead.courseservice.models.LessonModel;
 import com.ead.courseservice.models.ModuleModel;
 import com.ead.courseservice.repositories.CourseRepository;
+import com.ead.courseservice.repositories.CourseUserRepository;
 import com.ead.courseservice.repositories.LessonRepository;
 import com.ead.courseservice.repositories.ModuleRepository;
 import com.ead.courseservice.services.CourseService;
@@ -29,11 +31,13 @@ public class CourseServiceImpl implements CourseService {
     final CourseRepository courseRepository;
     final ModuleRepository moduleRepository;
     final LessonRepository lessonRepository;
+    final CourseUserRepository courseUserRepository;
 
-    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository) {
+    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository, CourseUserRepository courseUserRepository) {
         this.courseRepository = courseRepository;
         this.moduleRepository = moduleRepository;
         this.lessonRepository = lessonRepository;
+        this.courseUserRepository = courseUserRepository;
     }
 
     @Transactional
@@ -48,6 +52,10 @@ public class CourseServiceImpl implements CourseService {
                 }
             }
             moduleRepository.deleteAll(moduleModelList);
+        }
+        List<CourseUserModel> courseUserModelList = courseUserRepository.findAllCourseUserIntoCourse(courseModel.getCourseId());
+        if(!courseUserModelList.isEmpty()) {
+            courseUserRepository.deleteAll(courseUserModelList);
         }
         courseRepository.delete(courseModel);
     }
--- EAD/course-service/src/main/java/com/ead/courseservice/validations/CourseValidator.java
+++ EAD/course-service/src/main/java/com/ead/courseservice/validations/CourseValidator.java
@@ -8,7 +8,6 @@ import com.ead.courseservice.services.CourseService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
