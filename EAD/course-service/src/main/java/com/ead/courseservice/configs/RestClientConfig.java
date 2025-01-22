f7d2551d5daf9ed24cf7135fdfdcd7bd43c3590f
Ramon <ramonsilva12305@outlook.com>
2025-01-21 23:25:41 -0300

    feat/Discovery Pattern Implementation

--- EAD/course-service/src/main/java/com/ead/courseservice/configs/RestClientConfig.java
+++ EAD/course-service/src/main/java/com/ead/courseservice/configs/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.ead.courseservice.configs;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    @LoadBalanced
     @Bean
     public RestClient.Builder RestClientBuilder() {
         return RestClient.builder();
