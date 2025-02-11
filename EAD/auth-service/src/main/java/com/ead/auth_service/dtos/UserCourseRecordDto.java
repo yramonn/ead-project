a7aa0c29b71f8a0f7f27e86aa9b783e782357b79
Ramon <ramonsilva12305@outlook.com>
2025-02-10 21:46:42 -0300

    refact/Deleting sync communication pattern for refactor with async event driven pattern

--- EAD/auth-service/src/main/java/com/ead/auth_service/dtos/UserCourseRecordDto.java
+++
@@ -1,9 +0,0 @@
-package com.ead.auth_service.dtos;
-
-import jakarta.validation.constraints.NotNull;
-
-import java.util.UUID;
-
-public record UserCourseRecordDto(UUID userId,
-                                  @NotNull(message = "CourseId is mandatory") UUID courseId) {
-}
