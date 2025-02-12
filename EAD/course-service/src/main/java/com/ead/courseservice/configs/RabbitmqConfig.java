d7e6b84867ed760f54bc2557dfc16b15b3cca0e1
Ramon <ramonsilva12305@outlook.com>
2025-02-12 00:24:43 -0300

    feat/Initialize RabbitMQConfig with Spring AMQP

---
+++ EAD/course-service/src/main/java/com/ead/courseservice/configs/RabbitmqConfig.java
@@ -0,0 +1,33 @@
+package com.ead.courseservice.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    final CachingConnectionFactory connectionFactory;
+
+    public RabbitmqConfig(CachingConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
