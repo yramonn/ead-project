8c89006ae58e6139295bb7165318ca7618e804b0
Ramon <ramonsilva12305@outlook.com>
2025-06-23 20:37:51 -0300

    feat: Access token pattern implementation for notification microservice

---
+++ EAD/notification-service/src/main/java/com/ead/notificationservice/configs/security/AuthenticationJwtFilter.java
@@ -0,0 +1,55 @@
+package com.ead.notificationservice.configs.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class AuthenticationJwtFilter extends OncePerRequestFilter {
+
+    Logger logger = LogManager.getLogger(AuthenticationJwtFilter.class);
+
+    final JwtProvider jwtProvider;
+
+    public AuthenticationJwtFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwtStr = getTokenHeader(request);
+            if(jwtStr != null && jwtProvider.validateJwt(jwtStr)) {
+                String userId = jwtProvider.getSubjectFromJwt(jwtStr);
+                String roleStr = jwtProvider.getClaimNameFromJwt(jwtStr, "roles");
+                UserDetails userDetails = UserDetailsImpl.build(UUID.fromString(userId), roleStr);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch(Exception e) {
+            logger.error("Cannot set User Auth: {}", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenHeader(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
+    }
+}
--- EAD/notification-service/src/main/java/com/ead/notificationservice/controllers/UserNotificationController.java
+++ EAD/notification-service/src/main/java/com/ead/notificationservice/controllers/UserNotificationController.java
@@ -1,5 +1,7 @@
 package com.ead.notificationservice.controllers;
 
+import com.ead.notificationservice.configs.security.AuthenticationCurrentUserService;
+import com.ead.notificationservice.configs.security.UserDetailsImpl;
 import com.ead.notificationservice.dtos.NotificationRecordDto;
 import com.ead.notificationservice.models.NotificationModel;
 import com.ead.notificationservice.services.NotificationService;
@@ -8,6 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -16,26 +21,37 @@ import java.util.UUID;
 public class UserNotificationController {
 
     final NotificationService notificationService;
+    final AuthenticationCurrentUserService authenticationCurrentUserService;
 
-    public UserNotificationController(NotificationService notificationService) {
+    public UserNotificationController(NotificationService notificationService, AuthenticationCurrentUserService authenticationCurrentUserService) {
         this.notificationService = notificationService;
+        this.authenticationCurrentUserService = authenticationCurrentUserService;
     }
 
+    @PreAuthorize("hasAnyRole('USER')")
     @GetMapping("/users/{userId}/notifications")
     public ResponseEntity<Page<NotificationModel>> getAllNotificationsByUser(@PathVariable(value = "userId") UUID userId,
                                                                              Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(notificationService.findAllNotificationsByUser(userId, pageable));
+        UserDetailsImpl userDetails = authenticationCurrentUserService.getCurrentUser();
+        if(userDetails.getUserId().equals(userId) || userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(notificationService.findAllNotificationsByUser(userId, pageable));
+        } else {
+            throw new AccessDeniedException("Forbidden");
+        }
     }
 
+    @PreAuthorize("hasAnyRole('USER')")
     @PutMapping("/users/{userId}/notifications/{notificationId}")
     public ResponseEntity<Object> updateNotification(@PathVariable(value = "userId") UUID userId,
                                                      @PathVariable(value = "notificationId") UUID notificationId,
                                                      @RequestBody @Valid NotificationRecordDto notificationRecordDto) {
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(notificationService.updateNotification(notificationRecordDto, notificationService.findByNotificationIdAndUserId(notificationId, userId).get()));
-
+        UserDetailsImpl userDetails = authenticationCurrentUserService.getCurrentUser();
+        if(userDetails.getUserId().equals(userId) || userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(notificationService.updateNotification(notificationRecordDto, notificationService.findByNotificationIdAndUserId(notificationId, userId).get()));
+        } else {
+            throw new AccessDeniedException("Forbidden");
+        }
     }
-
 }
