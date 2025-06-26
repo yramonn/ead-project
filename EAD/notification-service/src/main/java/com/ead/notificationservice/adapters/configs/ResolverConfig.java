5bd72290195e55e68536cfbc2d381f0a0c5ce8a3
Ramon <ramonsilva12305@outlook.com>
2025-06-25 23:35:04 -0300

    feat: Starting Hexagonal arch

---
+++ EAD/notification-service/src/main/java/com/ead/notificationservice/adapters/configs/ResolverConfig.java
@@ -0,0 +1,37 @@
+package com.ead.notificationservice.adapters.configs;
+
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+public class ResolverConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationArgumentResolver());
+
+        var pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setFallbackPageable(PageRequest.of(0, 10));
+        argumentResolvers.add(pageableResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/users/**").allowedOrigins("https");
+//        registry.addMapping("/auth/**").allowedOrigins("https");
+        registry.addMapping("/**").maxAge(3600);
+
+    }
+
+}
---
+++ EAD/notification-service/src/main/java/com/ead/notificationservice/adapters/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.ead.notificationservice.adapters.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
